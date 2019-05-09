8d863596a64d64b77f1d22b152041045a285befb
Reina Ewing <reina.castro3@gmail.com>
2019-05-09 14:30:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [ 
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+  ]
+  
+  win_combintation1
+
+def won?(board)
+   board[0] == "X" &&
+     board[1] == "X" &&
+     board[2] == "X"
+     puts "X wins in the 1st row."
+    else
+      puts " "
+    end
+  end
